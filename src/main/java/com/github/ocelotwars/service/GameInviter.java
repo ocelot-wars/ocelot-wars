21cad8405b9f5944423567c8fdf6ddceb0de0809
bfeigl <bfeigl@gmx.de>
2017-05-07 23:21:27 +0200

    tests for GameInviter

--- src/main/java/com/github/ocelotwars/service/GameInviter.java
+++ src/main/java/com/github/ocelotwars/service/GameInviter.java
@@ -17,12 +17,20 @@ import io.vertx.core.net.SocketAddress;
 
 public class GameInviter {
 	private static final String ACCEPT = "accept";
-	private static final int MINIMAL_PLAYER_COUNT = 1;
+	private static final int DEFAULT_MINIMAL_PLAYER_COUNT = 1;
+
 	private HttpClient httpClient;
-	private List<Player> participatingPlayers;
 	private ScheduledExecutorService executor;
-	private List<Player> registeredPlayers;
 	private Runnable waitForPlayersRunnable;
+
+	private List<Player> participatingPlayers = new ArrayList<>();
+	private List<Player> registeredPlayers = new ArrayList<>();
+	private int minimalPlayerCount = DEFAULT_MINIMAL_PLAYER_COUNT;
+
+	protected GameInviter(HttpClient client, Runnable waitForPlayersRunnable, ScheduledExecutorService executor) {
+		this(client, waitForPlayersRunnable);
+		this.executor = executor;
+	}
 
 	public GameInviter(HttpClient client, Runnable waitForPlayersRunnable) {
 		this.httpClient = client;
@@ -35,27 +43,29 @@ public class GameInviter {
 		System.out.println("invitation started");
 		this.registeredPlayers = new ArrayList<>(registeredPlayers);
 		participatingPlayers = new ArrayList<>();
-		registeredPlayers.stream().forEach(player -> httpClient
-				.post(player.getPort(), player.getHost(), "/invite", this::invitationResponse).end());
+		registeredPlayers.stream().forEach(player -> {
+			httpClient
+				.post(player.getPort(), player.getHost(), "/invite", this::invitationResponse).end();
+		});
 		executor.schedule(this::checkStartGame, 5, TimeUnit.SECONDS);
 	}
 
-	private void checkStartGame() {
+	protected void checkStartGame() {
 		// debugging-output
 		System.out.println("participating players: " + participatingPlayers.size());
-		if (participatingPlayers.size() >= MINIMAL_PLAYER_COUNT) {
+		if (participatingPlayers.size() >= minimalPlayerCount) {
 			new GameRunner(participatingPlayers).start();
 		} else {
 			waitForPlayersRunnable.run();
 		}
 	}
 
-	private void invitationResponse(HttpClientResponse response) {
+	protected void invitationResponse(HttpClientResponse response) {
 		if (response.statusCode() == OK.code() && response.statusMessage().equals(ACCEPT)) {
 			SocketAddress remoteAddress = response.netSocket().remoteAddress();
 			Optional<Player> player = getPlayerByAddress(remoteAddress);
 			if (player.isPresent()) {
-				// TODO: überprüfen, dass der Player nicht schon da ist
+				// TODO: Ã¼berprÃ¼fen, dass der Player nicht schon da ist
 				participatingPlayers.add(player.get());
 			}
 		}
@@ -65,7 +75,15 @@ public class GameInviter {
 		String playerHost = remoteAddress.host();
 		int playerPort = remoteAddress.port();
 		return registeredPlayers.stream()
-				.filter(player -> player.getHost().equals(playerHost) && player.getPort() == playerPort).findFirst();
+			.filter(player -> player.getHost().equals(playerHost) && player.getPort() == playerPort).findFirst();
+	}
+
+	protected List<Player> getParticipatingPlayers() {
+		return participatingPlayers;
+	}
+
+	protected void setMinimalPlayerCount(int minimalPlayerCount) {
+		this.minimalPlayerCount = minimalPlayerCount;
 	}
 
 }
---
+++ src/test/java/com/github/ocelotwars/service/GameInviterTest.java
@@ -0,0 +1,138 @@
+package com.github.ocelotwars.service;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.github.ocelotwars.engine.Player;
+
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpClientRequest;
+import io.vertx.core.http.HttpClientResponse;
+import io.vertx.core.net.impl.SocketAddressImpl;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GameInviterTest {
+
+	private static final String OTHER_HOST = "other_host";
+	private static final long TIMEOUT_WAIT_FOR_INVITATIONS = 5L;
+	private static final String NAME = "name";
+	private static final String HOST = "host";
+	private static final int PORT = 1234;
+
+	@Mock
+	private HttpClient httpClient;
+
+	@Mock
+	private ScheduledExecutorService executor;
+
+	@Mock
+	private Runnable waitForPlayersRunnable;
+
+	@InjectMocks
+	private GameInviter gameInviter;
+
+	@Before
+	public void setUp() {
+		gameInviter = new GameInviter(httpClient, waitForPlayersRunnable, executor);
+	}
+
+	@Test
+	public void testInviteToGame() throws Exception {
+		Player player = new Player(NAME, HOST, PORT);
+		HttpClientRequest httpClientRequest = setupMocksHttpClient();
+
+		gameInviter.inviteToGame(asList(player));
+
+		verify(httpClient).post(Mockito.eq(PORT), Mockito.eq(HOST), Mockito.eq("/invite"), Mockito.any());
+		verify(httpClientRequest).end();
+		verify(executor).schedule(Mockito.any(Runnable.class), Mockito.eq(TIMEOUT_WAIT_FOR_INVITATIONS), Mockito.eq(TimeUnit.SECONDS));
+	}
+
+	private HttpClientRequest setupMocksHttpClient() {
+		HttpClientRequest httpClientRequest = Mockito.mock(HttpClientRequest.class);
+		when(httpClient.post(Mockito.eq(PORT), Mockito.eq(HOST), Mockito.eq("/invite"), Mockito.any())).thenReturn(httpClientRequest);
+		return httpClientRequest;
+	}
+
+	@Test
+	public void testInvitationResponse_wrongResponseCode() throws Exception {
+		HttpClientResponse httpClientResponse = Mockito.mock(HttpClientResponse.class);
+		when(httpClientResponse.statusCode()).thenReturn(404);
+
+		gameInviter.invitationResponse(httpClientResponse);
+
+		assertThat(gameInviter.getParticipatingPlayers(), Matchers.is(Matchers.empty()));
+	}
+
+	@Test
+	public void testInvitationResponse_wrongResponseMessage() throws Exception {
+		HttpClientResponse httpClientResponse = Mockito.mock(HttpClientResponse.class);
+		when(httpClientResponse.statusCode()).thenReturn(200);
+		when(httpClientResponse.statusMessage()).thenReturn("wrong");
+
+		gameInviter.invitationResponse(httpClientResponse);
+
+		assertThat(gameInviter.getParticipatingPlayers(), Matchers.is(Matchers.empty()));
+	}
+
+	@Test
+	public void testInvitationResponse_playerNotFound() throws Exception {
+		HttpClientResponse httpClientResponse = Mockito.mock(HttpClientResponse.class, Mockito.RETURNS_DEEP_STUBS);
+		when(httpClientResponse.statusCode()).thenReturn(200);
+		when(httpClientResponse.statusMessage()).thenReturn("accept");
+		when(httpClientResponse.netSocket().remoteAddress()).thenReturn(new SocketAddressImpl(PORT, OTHER_HOST));
+
+		gameInviter.invitationResponse(httpClientResponse);
+
+		assertThat(gameInviter.getParticipatingPlayers(), Matchers.is(Matchers.empty()));
+	}
+
+	@Test
+	public void testInvitationResponse_playerFound() throws Exception {
+		Player player = new Player(NAME, HOST, PORT);
+		setupMocksHttpClient();
+		gameInviter.inviteToGame(asList(player));
+
+		HttpClientResponse httpClientResponse = Mockito.mock(HttpClientResponse.class, Mockito.RETURNS_DEEP_STUBS);
+		when(httpClientResponse.statusCode()).thenReturn(200);
+		when(httpClientResponse.statusMessage()).thenReturn("accept");
+		when(httpClientResponse.netSocket().remoteAddress()).thenReturn(new SocketAddressImpl(PORT, HOST));
+
+		gameInviter.invitationResponse(httpClientResponse);
+
+		assertThat(gameInviter.getParticipatingPlayers(), Matchers.containsInAnyOrder(player));
+	}
+
+	@Test
+	public void testCheckStartGame_notEnoughPlayers() throws Exception {
+		gameInviter.setMinimalPlayerCount(1);
+
+		gameInviter.checkStartGame();
+
+		verify(waitForPlayersRunnable).run();
+	}
+
+	@Test
+	public void testCheckStartGame_enoughPlayers() throws Exception {
+		gameInviter.setMinimalPlayerCount(0);
+
+		gameInviter.checkStartGame();
+
+		Mockito.verifyZeroInteractions(waitForPlayersRunnable);
+	}
+
+}
